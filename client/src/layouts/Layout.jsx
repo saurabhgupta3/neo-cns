c164375f7bdb07ebe7d23cf6a842df3ac17eb3c1
Saurabh Gupta <sggupta548@gmail.com>
2026-01-06 00:05:15 +0530

    add footer

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,13 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import "./Layout.css";
 
 export default function Layout() {
     return (
-        <>
+        <div className="page">
             <Navbar></Navbar>
             <h1>Courier Network System</h1>
             <hr />
-            <Outlet></Outlet>
-        </>
+            <main className="content">
+                <Outlet></Outlet>
+            </main>
+            <Footer></Footer>
+        </div>
     )
 }
