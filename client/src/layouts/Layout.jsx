28092df69ea9f3de6e4860bbf20c2db01f0f9ab9
Saurabh Gupta <sggupta548@gmail.com>
2026-01-07 00:32:06 +0530

    a little change

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -7,6 +7,8 @@ export default function Layout() {
     return (
         <div className="page">
             <Navbar></Navbar>
+            <h1>Courier Network System</h1>
+            <hr />
             <main className="content">
                 <Outlet></Outlet>
             </main>
